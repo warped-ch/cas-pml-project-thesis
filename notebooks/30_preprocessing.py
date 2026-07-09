ed1028b5f8a69cc51db2ac456716fde5e17172ac
Andreas Wiedmer <wiedmer.a@gmail.com>
2026-07-09 20:31:53 +0200

    + improve 2D view rendering, save images to temp dir

--- notebooks/30_preprocessing.py
+++ notebooks/30_preprocessing.py
@@ -22,6 +22,7 @@ import yaml
 from pathlib import Path
 from pytorch3d.io import IO
 from pytorch3d.renderer import (
+    BlendParams,
     DirectionalLights,
     look_at_view_transform,
     camera_position_from_spherical_angles,
@@ -99,13 +100,25 @@ plot_mesh(mesh)
 # %%
 # render 2D projection
 
-elevation = np.array([0, 60, 45, 45, 315, 315])
-azimuth = np.array([0, 0, 45, 315, 45, 315])
+# each row in views is [elevation, azimuth]
+views = np.array([
+    [0, 0], # top down
+    [-90, 0], # front center
+    [90, 0], # back center
+    [-60, 0], # top down front diagonal center
+    [60, 0], # top down back diagonal center
+    [0, -45], # top down front diagonal left
+    [0, 45], # top down front diagonal right
+    [60, -45], # bottom up back diagonal left
+    [60, 45], # bottom up back diagonal right
+    [0, -90], # left side
+    [0, 90], # right side
+])
 
 R, T = look_at_view_transform(
     dist=80,
-    elev=elevation,
-    azim=azimuth,
+    elev=views[:, 0],
+    azim=views[:, 1],
     device=device
 )
 
@@ -122,8 +135,7 @@ raster_settings = RasterizationSettings(
     image_size=(512, 512)
 )
 
-light_dir = camera_position_from_spherical_angles(distance=1.0, elevation=elevation, azimuth=azimuth, device=device)
-print(f"light_dir={light_dir}")
+light_dir = camera_position_from_spherical_angles(distance=1.0, elevation=views[:, 0], azimuth=views[:, 1], device=device)
 
 lights = DirectionalLights(
     direction=light_dir,
@@ -133,6 +145,8 @@ lights = DirectionalLights(
     device=device
 )
 
+blend_params = BlendParams(background_color=(1.0, 1.0, 1.0))
+
 renderer = MeshRenderer(
     rasterizer=MeshRasterizer(
         cameras=cameras, 
@@ -141,19 +155,33 @@ renderer = MeshRenderer(
     shader=SoftPhongShader(
         cameras=cameras,
         lights=lights,
+        blend_params=blend_params,
         device=device
     )
 )
 
-meshes = mesh.extend(elevation.size)
+meshes = mesh.extend(views.shape[0])
 images = renderer(meshes)
 
-cols = min(2, elevation.size)
-rows = math.ceil(elevation.size / cols)
+temp_out_path = dataset_path.parent / "temp" / obj_file.stem
+print(f"temp_out_path={temp_out_path}")
+temp_out_path.mkdir(parents=True, exist_ok=True)
+
+cols = min(3, views.shape[0])
+rows = math.ceil(views.shape[0] / cols)
 f, axarr = plt.subplots(rows, cols, figsize=(12, 12))
 for i, ax in enumerate(axarr.flat):
     if i < images.shape[0]:
-        ax.imshow(images[i].detach().cpu().numpy())
-        ax.set_title(f"elevation={elevation[i]}, azimuth={azimuth[i]}") 
+        # slice [..., :3] to remove alpha channel
+        img_np = images[i].detach().cpu().numpy()[..., :3]
+
+        elev = views[i, 0].item()
+        azim = views[i, 1].item()
+
+        ax.imshow(img_np)
+        ax.set_title(f"elevation={elev}, azimuth={azim}")
+
+        plt.imsave(temp_out_path / f"view_2d_evev{elev}_azim{azim}.png", img_np)
+
 plt.tight_layout()
 plt.show()
