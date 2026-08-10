8b11c20a161fc2d8990f14a8879f02d236f512dd
Andreas Wiedmer <wiedmer.a@gmail.com>
2026-08-10 21:05:38 +0200

    + train for 100 epochs with adapted grad_accum_steps=8 and lr=2.5e-5 (linear scaling rule)

--- notebooks/40_train_rfdetr.py
+++ notebooks/40_train_rfdetr.py
@@ -37,52 +37,6 @@ dataset_path_2d = root_path / config.get("dataset_path_2d")
 print(f"dataset_path_2d={dataset_path_2d}")
 
 # %%
-# TEMP create smaller datasets for testing
-
-import fiftyone as fo
-
-num_train_samples = 1000
-num_test_samples = (int)(0.2*num_train_samples)
-
-dataset_test_path = root_path / "data" / "Teeth2D_test"
-
-shutil.rmtree(dataset_test_path, ignore_errors=True)
-
-def export_split(view, path):
-    print(f"exporting dataset split: {path}")
-    view.export(
-        dataset_type=fo.types.COCODetectionDataset,
-        export_dir=str(path),
-        labels_path="_annotations.coco.json",
-        data_path=str(path),
-        label_field="ground_truth_det",
-        export_media=True,
-        abs_paths=False,
-        overwrite=True,
-        # TODO: should use: tolerance=0, # Keeps every pixel boundary point
-    )
-
-dataset = fo.load_dataset("Teeth2D")
-
-train_view = dataset.match_tags("train").limit(num_train_samples)
-train_path = dataset_test_path / "train"
-export_split(train_view, train_path)
-
-test_view = dataset.match_tags("test").limit(num_test_samples)
-test_path = dataset_test_path / "test"
-export_split(test_view, test_path)
-
-valid_path = dataset_test_path / "valid"
-valid_path.mkdir(parents=True, exist_ok=True)
-valid_dummy = {
-    "images": [],
-    "annotations": [],
-    "categories": []
-}
-with open(valid_path / "_annotations.coco.json", "w", encoding="utf-8") as f:
-    json.dump(valid_dummy, f)
-
-# %%
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
 # TODO: link this to "image_size" in config
@@ -94,9 +48,9 @@ output_dir = root_path / config["output_rf_detr_train"] / timestamp
 # https://rfdetr.roboflow.com/latest/learn/train/training-parameters/#understanding-batch-size
 model.train(
     dataset_dir=str(dataset_path_2d),
-    epochs=50,
+    epochs=100,
     batch_size=4,
-    grad_accum_steps=16,
-    lr=5e-5,
+    grad_accum_steps=8,
+    lr=2.5e-5,
     output_dir=output_dir,
 )
