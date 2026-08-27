582e2c3137c57cdace3a96fdb88f2d83bc42bf59
Andreas Wiedmer <wiedmer.a@gmail.com>
2026-08-27 19:55:49 +0200

    + set learning rate back to default

--- notebooks/40_train_rfdetr.py
+++ notebooks/40_train_rfdetr.py
@@ -68,7 +68,7 @@ for dataset_path in dataset_paths:
         epochs=200,
         batch_size=8,
         grad_accum_steps=2,
-        lr=5e-5,
+        lr=1e-4,
         aug_config={},  # disable horizontal flip while keeping required resizing and normalization
         save_dataset_grids=True,
         # TODO: speed up training by specifying "num_queries" according to classes in dataset?
