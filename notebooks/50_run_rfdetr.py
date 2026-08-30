720154987e11270153ebbc9120c42738b781dd46
Andreas Wiedmer <wiedmer.a@gmail.com>
2026-08-30 20:00:38 +0200

    % remove debug code

--- notebooks/50_run_rfdetr.py
+++ notebooks/50_run_rfdetr.py
@@ -110,34 +110,3 @@ nb_utils.plot_image_grid(
     images=annotated_images,
     titles=[f"elevation={view[0]}, azimuth={view[1]}" for view in views],
 )
-
-# %%
-from rfdetr import RFDETRSegMedium
-
-model = RFDETRSegMedium(pretrain_weights=str(best_model_chkpt_path), device=device)
-
-image = Path(
-    r"C:\Development\cas_pml\project_thesis\data\Teeth2D_lower_has_model_base_false\test\ZM8PCSK6_lower_elev-60_azim0.png"
-)
-detections = model.predict(str(image), threshold=0.5)
-
-annotated_image = mask_annotator.annotate(
-    detections.metadata["source_image"], detections
-)
-cv2.imwrite(
-    temp_out_path / f"mask_annotator1.png",
-    cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR),
-)
-
-labels = [
-    model.class_names[class_id].replace("class_", "")
-    for class_id in detections.class_id
-]
-annotated_image = sv.LabelAnnotator(
-    text_position=sv.Position.CENTER, text_padding=0
-).annotate(annotated_image, detections, labels)
-cv2.imwrite(
-    temp_out_path / f"mask_annotator2.png",
-    cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR),
-)
-
