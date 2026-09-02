c54e49831c5c3e5cb079d6fc560df1dc3f5fbe70
Andreas Wiedmer <wiedmer.a@gmail.com>
2026-09-02 11:48:34 +0200

    - improve handling on multiple predictions on same tooth (class agnostic NMS, consider only best detection per class)

---
+++ notebooks/90_debug_run.py
@@ -0,0 +1,130 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.19.4
+#   kernelspec:
+#     display_name: project-thesis (3.12.9)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Debugging inference issues
+
+# %%
+import random
+import sys
+from pathlib import Path
+
+import cv2
+import matplotlib.pyplot as plt
+import notebook_utils as nb_utils
+import numpy as np
+import seaborn as sns
+import supervision as sv
+import torch
+import yaml
+
+sys.path.append(str(Path.cwd().parent))
+from src import inference_pipeline
+
+root_path = Path.cwd().parent
+print(f"root_path={root_path}")
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
+
+# %%
+# load config file
+
+with open("../config/config.yaml", "r") as f:
+    config = yaml.safe_load(f)
+
+dataset_path_3d = root_path / config.get("dataset_path_3d")
+print(f"dataset_path_3d={dataset_path_3d}")
+
+best_model_chkpt_path = root_path / config.get("best_model_chkpt_path")
+print(f"best_model_chkpt_path={best_model_chkpt_path}")
+
+views = np.array(config["2d_projection"]["views"])
+
+ip = inference_pipeline.InferencePipeline(
+    chkpt_file=str(best_model_chkpt_path), config=config, device=device
+)
+print(f"model.class_names: {ip.model.class_names}")
+print(f"model.model_config.resolution: {ip.model.model_config.resolution}")
+
+# %%
+colors = nb_utils.get_colors()
+print(f"colors: {len(colors)}, {colors}")
+
+class_ids = config["class_ids"]
+sns.palplot(colors)
+plt.title("FDI class_id color labels", fontsize=16, pad=20)
+plt.xticks(range(len(colors)), class_ids)
+plt.show()
+
+colors_pv = nb_utils.convert_colors_pv(colors, config["class_ids"])
+print(f"colors_pv: {len(colors_pv)}, {colors_pv}")
+colors_sv = nb_utils.convert_colors_sv(colors)
+print(f"colors_sv: {len(colors_sv)}, {colors_sv}")
+
+# %%
+obj_file = dataset_path_3d / "raw/lower/01KRDUKX/01KRDUKX_lower.obj"
+print(f"obj_file={obj_file}")
+
+temp_out_path = dataset_path_3d.parent / "temp" / Path(obj_file).stem
+print(f"temp_out_path={temp_out_path}")
+temp_out_path.mkdir(parents=True, exist_ok=True)
+
+mesh, vertex_labels = ip.run_inference(obj_file)
+
+for i, det in enumerate(ip.detections):
+    cv2.imwrite(temp_out_path / f"image_{i}.png", det.metadata["source_image"])
+for i, mask in enumerate(ip.masks):
+    cv2.imwrite(temp_out_path / f"mask_{i}.png", mask)
+
+# %%
+mask_annotator = sv.MaskAnnotator(color=colors_sv)
+label_annotator = sv.LabelAnnotator(
+    color=colors_sv,
+    text_color=sv.Color.BLACK,
+    text_scale=0.4,
+    text_padding=0,
+    text_position=sv.Position.CENTER,
+)
+
+for i, det in enumerate(ip.detections):
+    print(
+        f"det[{i}]: class_ids={det.class_id}, fdi_labels={[class_name.replace('class_', '') for class_name in det['class_name']]}"
+    )
+
+    for class_id in det.class_id:
+        det_filter = det[det.class_id == class_id]
+        class_name = det_filter["class_name"]
+        if len(class_name) > 1:
+            print(f"⚠️ multiple objects for same class: class_name={class_name}")
+
+        labels = [
+            f"{name} {conf:.2f}"
+            for name, conf in zip(det_filter["class_name"], det_filter.confidence)
+        ]
+
+        annotated_img = mask_annotator.annotate(
+            scene=det.metadata["source_image"].copy(), detections=det_filter
+        )
+        annotated_img = label_annotator.annotate(
+            scene=annotated_img, detections=det_filter, labels=labels
+        )
+
+        annotated_img_file_name = f"annotated_elevation_{views[i][0]}_azimuth_{views[i][1]}_{class_name[0]}.png"
+        # allows better file ordering
+        annotated_img_file_name = annotated_img_file_name.replace("-", "neg")
+        cv2.imwrite(
+            temp_out_path / annotated_img_file_name,
+            annotated_img,
+        )
--- src/inference_pipeline.py
+++ src/inference_pipeline.py
@@ -31,6 +31,7 @@ class InferencePipeline:
             self.model = RFDETRSegMedium.from_checkpoint(path=chkpt_file, device=device)
 
         # TODO: temp stuff for debugging
+        self.detections_raw = None
         self.detections = None
         self.mask = None
 
@@ -44,6 +45,7 @@ class InferencePipeline:
     def run_inference(
         self, obj_file: str, threshold: float = 0.5
     ) -> tuple[Meshes, NDArray[np.uint8]]:
+        self.detections_raw = None
         self.detections = None
         self.masks = None
 
@@ -62,18 +64,44 @@ class InferencePipeline:
                 f"run_inference: images.shape={images.shape}, images.dim={images.dim()}"
             )
 
-        self.detections = self.model.predict(
+        self.detections_raw = self.model.predict(
             images=list(images),
             threshold=threshold,
         )
 
+        # Filter detections to keep only the best detection per class (highest confidence)
+        # TODO: this will break "Teeth3DS+\raw\upper\SJDH33M1\SJDH33M1_upper.obj"
+        self.detections = []
+        for det_raw in self.detections_raw:
+            # apply class agnostic NMS to the individual detections
+            # NMS: https://github.com/roboflow/rf-detr/issues/187
+            # TODO: consider using with_nmm?
+            # https://supervision.roboflow.com/develop/detection/core/#supervision.detection.core.Detections.with_nmm
+            det = det_raw.with_nms(threshold=0.5, class_agnostic=True)
+
+            if len(det) == 0:
+                self.detections.append(det)
+                continue
+
+            best_class_indices = []
+            # loop the detections of one view image by class id
+            for class_id in np.unique(det.class_id):
+                # find indices for this specific class_id
+                class_indices = np.where(det.class_id == class_id)[0]
+                # find index of the detection with highest confidence for this class
+                best_idx = class_indices[np.argmax(det.confidence[class_indices])]
+                best_class_indices.append(best_idx)
+            # keep only the best detection per class
+            self.detections.append(det[np.array(best_class_indices)])
+
         # TODO: potential bug (removes gingiva, only teeth labels survive, rest is 0)
-        # TODO: avoid CPU roundtrip?
         self.masks = []
-        for det in self.detections:
-            _, h, w = det.mask.shape
+        # loop the filtered detections of all the view images
+        for det_view in self.detections:
+            _, h, w = det_view.mask.shape
             combined_mask = np.zeros((h, w), dtype=np.uint8)
-            for mask, class_id in zip(det.mask, det.class_id):
+            # loop the filtered detections of one view image by class id
+            for mask, class_id in zip(det_view.mask, det_view.class_id):
                 mask_value = self.get_mask_value(class_id)
                 combined_mask[mask] = mask_value
             self.masks.append(combined_mask)
