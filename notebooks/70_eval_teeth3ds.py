ed7da40e0f2d9ea9314953b98fc73da85f85e8ab
Andreas Wiedmer <wiedmer.a@gmail.com>
2026-09-16 19:03:07 +0200

    + improve Teeth3DS eval notebook

--- notebooks/70_eval_teeth3ds.py
+++ notebooks/70_eval_teeth3ds.py
@@ -42,139 +42,150 @@ print(f"Using device: {device}")
 
 config = nb_utils.load_config()
 
-dataset_path_2d = nb_utils.resolve_config_path("dataset_path_2d", config)
-print(f"dataset_path_2d={dataset_path_2d}")
 dataset_path_3d = nb_utils.resolve_config_path("dataset_path_3d", config)
 print(f"dataset_path_3d={dataset_path_3d}")
-
-best_model_chkpt_path = nb_utils.resolve_config_path("best_model_chkpt_path", config)
-print(f"best_model_chkpt_path={best_model_chkpt_path}")
-
-out_path = nb_utils.resolve_config_path("output_path", config)
-print(f"out_path={out_path}")
-out_path.mkdir(parents=True, exist_ok=True)
 
 # %%
 # run inference pipeline on test split and save metrics
 
-# best_model_chkpt_path = nb_utils.resolve_config_path("output_rf_detr_train", config) / "20260912_010327/Teeth2D_upper" / "checkpoint_best_ema.pth"
-# print(f"best_model_chkpt_path={best_model_chkpt_path}")
-# dataset_path_2d = nb_utils.resolve_config_path("data_path", config) / "Teeth2D_upper"
-# print(f"dataset_path_2d={dataset_path_2d}")
-
-timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-
-ip = inference_pipeline.InferencePipeline(
-    chkpt_file=str(best_model_chkpt_path), config=config, device=device
+training_output_path = (
+    nb_utils.resolve_config_path("output_rf_detr_train", config) / "20260915_185454"
 )
-print(f"model.class_names: {ip.model.class_names}")
-print(f"model.model_config.resolution: {ip.model.model_config.resolution}")
-# TODO: bake into inference pipeline
-views = np.array(config["2d_projection"]["views"])
-num_views = views.shape[0]
-ip.model.inference(compile=True, batch_size=num_views)
+training_runs = [f.name for f in training_output_path.iterdir() if f.is_dir()]
+print(f"training_runs={training_runs}")
 
-test_split_path = dataset_path_2d / "test"
-png_files = list(test_split_path.rglob("*.png"))
-test_split_sample_ids = prefixes = {"_".join(f.stem.split("_")[:2]) for f in png_files}
-print(f"test_split_sample_ids: {len(test_split_sample_ids)}")
+for training_run in training_runs:
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-predictions = []
-for sample_id in tqdm(test_split_sample_ids, desc="Running 3DTeethSeg evaluation"):
-    obj_file = str(next(dataset_path_3d.rglob(f"{sample_id}.obj")))
+    best_model_chkpt_path = (
+        training_output_path / training_run / "checkpoint_best_ema.pth"
+    )
+    print(f"best_model_chkpt_path={best_model_chkpt_path}")
 
-    try:
-        mesh, vertex_labels, instances = ip.run_inference(obj_file)
-    except Exception as e:
-        print(f"💥 exception caught while processing {obj_file}: {str(e)}")
+    dataset_path_2d = nb_utils.resolve_config_path("data_path", config) / training_run
+    print(f"dataset_path_2d={dataset_path_2d}")
 
-    parts = sample_id.split("_")
-    id_patient = parts[0]
-    jaw = parts[1]
+    ip = inference_pipeline.InferencePipeline(
+        chkpt_file=str(best_model_chkpt_path), config=config, device=device
+    )
+    print(f"model.class_names: {ip.model.class_names}")
+    print(f"model.model_config.resolution: {ip.model.model_config.resolution}")
+    # TODO: bake into inference pipeline
+    views = np.array(config["2d_projection"]["views"])
+    num_views = views.shape[0]
+    ip.model.inference(compile=True, batch_size=num_views)
 
-    pred_label_dict = {
-        "id_patient": id_patient,
-        "jaw": jaw,
-        "labels": vertex_labels.tolist() if vertex_labels is not None else None,
-        "instances": instances.tolist() if instances is not None else None,
+    test_split_path = dataset_path_2d / "test"
+    png_files = list(test_split_path.rglob("*.png"))
+    test_split_sample_ids = prefixes = {
+        "_".join(f.stem.split("_")[:2]) for f in png_files
     }
-    predictions.append(pred_label_dict)
+    print(f"test_split_sample_ids: {len(test_split_sample_ids)}")
 
-metrics_dict = {
-    "model_chkpt_path": str(best_model_chkpt_path.relative_to(nb_utils.PROJECT_ROOT)),
-    "dataset_path_2d": str(dataset_path_2d.relative_to(nb_utils.PROJECT_ROOT)),
-    "dataset_path_3d": str(dataset_path_3d.relative_to(nb_utils.PROJECT_ROOT)),
-    "predictions": predictions,
-}
+    predictions = []
+    for sample_id in tqdm(test_split_sample_ids, desc="Running 3DTeethSeg evaluation"):
+        obj_file = str(next(dataset_path_3d.rglob(f"{sample_id}.obj")))
 
-metrics_file = out_path / f"3DTeethSeg_eval_{timestamp}.json"
-print(f"metrics_file={metrics_file}")
-with open(metrics_file, "w") as f:
-    json.dump(metrics_dict, f)
+        try:
+            mesh, vertex_labels, instances = ip.run_inference(obj_file)
+        except Exception as e:
+            print(f"💥 exception caught while processing {obj_file}: {str(e)}")
 
-# %%
-# read back the saved metrics and run 3DTeethSeg evaluation
+        parts = sample_id.split("_")
+        id_patient = parts[0]
+        jaw = parts[1]
 
-# metrics_file = out_path / "3DTeethSeg_eval_20260908_120309.json"
-with open(metrics_file, "r") as f:
-    metrics_dict = json.load(f)
+        pred_label_dict = {
+            "id_patient": id_patient,
+            "jaw": jaw,
+            "labels": vertex_labels.tolist() if vertex_labels is not None else None,
+            "instances": instances.tolist() if instances is not None else None,
+        }
+        predictions.append(pred_label_dict)
 
-obj_files = list(dataset_path_3d.rglob("*.obj"))
+    metrics_dict = {
+        "model_chkpt_path": str(
+            best_model_chkpt_path.relative_to(nb_utils.PROJECT_ROOT)
+        ),
+        "dataset_path_2d": str(dataset_path_2d.relative_to(nb_utils.PROJECT_ROOT)),
+        "dataset_path_3d": str(dataset_path_3d.relative_to(nb_utils.PROJECT_ROOT)),
+        "predictions": predictions,
+    }
 
-TLA, TSA, TIR = [], [], []
-for pred_label_dict in tqdm(metrics_dict["predictions"], desc="Evaluating predictions"):
-    try:
-        id_patient = pred_label_dict.get("id_patient")
-        jaw = pred_label_dict.get("jaw")
+    metrics_file = (
+        best_model_chkpt_path.parent
+        / f"3DTeethSeg_eval_{best_model_chkpt_path.stem}_{timestamp}.json"
+    )
+    print(f"metrics_file={metrics_file}")
+    with open(metrics_file, "w") as f:
+        json.dump(metrics_dict, f)
 
-        search_str = f"{id_patient}_{jaw}"
-        obj_file = next((f for f in obj_files if search_str in f.name), None)
-        gt_json_file = obj_file.with_suffix(".json")
+    # run 3DTeethSeg evaluation
 
-        mesh = trimesh.load(obj_file, process=False)
+    obj_files = list(dataset_path_3d.rglob("*.obj"))
 
-        with open(gt_json_file, "r") as f:
-            gt_label_dict = json.load(f)
+    TLA, TSA, TIR = [], [], []
+    for pred_label_dict in tqdm(
+        metrics_dict["predictions"], desc="Evaluating predictions"
+    ):
+        try:
+            id_patient = pred_label_dict.get("id_patient")
+            jaw = pred_label_dict.get("jaw")
 
-        gt_label_dict["mesh_vertices"] = np.array(mesh.vertices)
-        gt_label_dict["labels"] = np.array(gt_label_dict["labels"])
-        gt_label_dict["instances"] = np.array(gt_label_dict["instances"])
+            search_str = f"{id_patient}_{jaw}"
+            obj_file = next((f for f in obj_files if search_str in f.name), None)
+            gt_json_file = obj_file.with_suffix(".json")
 
-        pred_label_dict["labels"] = np.array(pred_label_dict["labels"])
-        pred_label_dict["instances"] = np.array(pred_label_dict["instances"])
+            mesh = trimesh.load(obj_file, process=False)
 
-        jaw_TLA, jaw_TSA, jaw_TIR = evaluation.calculate_metrics(
-            gt_label_dict, pred_label_dict
-        )
-        TLA.append(math.exp(-jaw_TLA))
-        TSA.append(jaw_TSA)
-        TIR.append(jaw_TIR)
-    except Exception as e:
-        print(
-            f"💥 exception caught while processing prediction {pred_label_dict}: {str(e)}"
-        )
-        TLA.append(0)
-        TSA.append(0)
-        TIR.append(0)
-        continue
+            with open(gt_json_file, "r") as f:
+                gt_label_dict = json.load(f)
 
-score = (np.mean(TSA) + np.mean(TLA) + np.mean(TIR)) / 3
-print("TSA : {} +- {}".format(np.mean(TSA), np.std(TSA)))
-print("TLA : {} +- {}".format(np.mean(TLA), np.std(TLA)))
-print("TIR : {} +- {}".format(np.mean(TIR), np.std(TIR)))
-print(" score : ", score)
+            gt_label_dict["mesh_vertices"] = np.array(mesh.vertices)
+            gt_label_dict["labels"] = np.array(gt_label_dict["labels"])
+            gt_label_dict["instances"] = np.array(gt_label_dict["instances"])
 
-# export metrics to /output/metrics.json
-score_dict = {
-    "global": score,
-    "TSA": np.mean(TSA),
-    "TLA": np.mean(TLA),
-    "TIR": np.mean(TIR),
-    "model_chkpt_path": str(best_model_chkpt_path.relative_to(nb_utils.PROJECT_ROOT)),
-    "dataset_path_2d": str(dataset_path_2d.relative_to(nb_utils.PROJECT_ROOT)),
-    "dataset_path_3d": str(dataset_path_3d.relative_to(nb_utils.PROJECT_ROOT)),
-}
+            pred_label_dict["labels"] = np.array(pred_label_dict["labels"])
+            pred_label_dict["instances"] = np.array(pred_label_dict["instances"])
 
-eval_metrics_file = metrics_file.parent / f"{metrics_file.stem}_results.json"
-with open(eval_metrics_file, "w") as f:
-    json.dump(score_dict, f)
+            jaw_TLA, jaw_TSA, jaw_TIR = evaluation.calculate_metrics(
+                gt_label_dict, pred_label_dict
+            )
+            TLA.append(math.exp(-jaw_TLA))
+            TSA.append(jaw_TSA)
+            TIR.append(jaw_TIR)
+        except Exception as e:
+            print(
+                f"💥 exception caught while processing prediction {pred_label_dict}: {str(e)}"
+            )
+            TLA.append(0)
+            TSA.append(0)
+            TIR.append(0)
+            continue
+
+    score = (np.mean(TSA) + np.mean(TLA) + np.mean(TIR)) / 3
+    print("TSA : {} +- {}".format(np.mean(TSA), np.std(TSA)))
+    print("TLA : {} +- {}".format(np.mean(TLA), np.std(TLA)))
+    print("TIR : {} +- {}".format(np.mean(TIR), np.std(TIR)))
+    print(" score : ", score)
+
+    # export metrics evaluation metrics
+
+    score_dict = {
+        "global": score,
+        "TSA": np.mean(TSA),
+        "TLA": np.mean(TLA),
+        "TIR": np.mean(TIR),
+        "model_chkpt_path": str(
+            best_model_chkpt_path.relative_to(nb_utils.PROJECT_ROOT)
+        ),
+        "dataset_path_2d": str(dataset_path_2d.relative_to(nb_utils.PROJECT_ROOT)),
+        "dataset_path_3d": str(dataset_path_3d.relative_to(nb_utils.PROJECT_ROOT)),
+    }
+
+    eval_metrics_file = (
+        best_model_chkpt_path.parent
+        / f"3DTeethSeg_eval_{best_model_chkpt_path.stem}_results_{timestamp}.json"
+    )
+    with open(eval_metrics_file, "w") as f:
+        json.dump(score_dict, f)
